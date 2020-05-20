5f153bd8e1fff074e2ce0faab0a87f0bc5957c2e
edin93 <il_matador@hotmail.fr>
2020-05-20 07:49:53 +0100

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -22,7 +22,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = square(width=9, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
